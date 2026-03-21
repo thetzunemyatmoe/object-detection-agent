b7e7b82b93f90d3281bc8fa13e01f2370dd6f676
Thet Myat Moe <tzm295@student.bham.ac.uk>
2026-03-21 23:23:13 +0000

    embedding/saving to vector db functions

--- util/rag_util.py
+++ util/rag_util.py
@@ -1,4 +1,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+from sentence_transformers import SentenceTransformer
 
 
 def chunk_documents(text, chunk_size=200, overlap=50):
@@ -9,3 +11,17 @@ def chunk_documents(text, chunk_size=200, overlap=50):
     )
     texts = text_splitter.split_text(text)
     return texts
+
+
+def embed(documents):
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    return model.encode(documents)
+
+
+def save_to_vector_db(embeddings):
+    client = chromadb.PersistentClient(path="data/vector_db")
+    collection = client.create_collection(name="collection")
+    collection.add(
+        ids=[f"id{index}" for index in range(1, len(embeddings) + 1)],
+        embeddings=embeddings
+    )
